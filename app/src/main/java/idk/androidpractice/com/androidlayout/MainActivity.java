9eb9ca5cd1518930db4f4554280a59017ebd0c71
Yukimee <leeling9696@gmail.com>
2016-10-04 15:10:47 +0800

    first commit

--- app/src/main/java/idk/androidpractice/com/androidlayout/MainActivity.java
+++ app/src/main/java/idk/androidpractice/com/androidlayout/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnUILayouts = (Button) findViewById(R.id.btnUILayouts);
         btnUILayouts.setOnClickListener(this);
 
+        //momoiioi
     }
 
     @Override
