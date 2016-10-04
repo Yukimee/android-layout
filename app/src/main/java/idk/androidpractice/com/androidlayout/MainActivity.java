2510df89eef4a8a1e337a4c632a1713176784f89
Yukimee <leeling9696@gmail.com>
2016-10-04 15:04:37 +0800

    first commit

---
+++ app/src/main/java/idk/androidpractice/com/androidlayout/MainActivity.java
@@ -0,0 +1,34 @@
+package idk.androidpractice.com.androidlayout;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnUILayouts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnUILayouts = (Button) findViewById(R.id.btnUILayouts);
+        btnUILayouts.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+        switch (view.getId()){
+            case R.id.btnUILayouts:
+                intent = new Intent(MainActivity.this, UILayoutsActivity.class);
+                startActivity(intent);
+                break;
+        }
+
+    }
+}
---
+++ app/src/main/java/idk/androidpractice/com/androidlayout/UILayoutsActivity.java
@@ -0,0 +1,13 @@
+package idk.androidpractice.com.androidlayout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class UILayoutsActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ui_layouts);
+    }
+}
